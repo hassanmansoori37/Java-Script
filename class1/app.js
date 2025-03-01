32e6bcb9fd66760659ee8363815c46715ee75f2e
class12 <mansoorihassan409@gmail.com>
2025-03-01 10:43:15 +0500

    Introduction JS

---
+++ class1/app.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+console.log("hassan");
+console.log("17");
+console.log("true");
+
+
+//Data typees
+
+//string "abchgkdk" "20" "false"
+//number 20,304935
+//boolean true | false
+
+
+//object
+//bigint
+//symbol
+//undefined
+//null
+
+console.log("Muhammad Hassan");
+console.log(17);
+console.log(true);
+
+
+console.log(typeof("hassan"));
+console.log(typeof(17));
+console.log(typeof(true));
+
+alert("Thanks For Watching");
