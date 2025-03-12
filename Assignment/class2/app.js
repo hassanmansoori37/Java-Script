9f246c04d511323afab40ec0530572774ce2eb49
class12 <mansoorihassan409@gmail.com>
2025-03-12 10:45:07 +0500

    varriable

---
+++ Assignment/class2/app.js
@@ -0,0 +1,67 @@
+var username = prompt("My Name");
+console.log (username);
+
+// ---------------------------------------------
+
+var message =("Hello World");
+alert (message);
+console.log (message);
+
+// ---------------------------------------------
+
+var username = ("Muhammad Hassan");
+var age = ("18");
+var certified = ("Certified Mobile Application Development");
+
+alert (username);
+alert (age);
+alert (certified);
+
+console.log(username);
+console.log(age);
+console.log(certified);
+
+// --------------------------------------------
+
+var text1 = ("PIZZA")
+var text2 = ("PIZZ")
+var text3 = ("PIZ")
+var text4 = ("PI")
+var text5 = ("P")
+
+var CombineText = text1 + "\n" + text2 +"\n" + text3 +"\n" + text4 + "\n" + text5;
+
+alert (CombineText)
+
+console.log(CombineText);
+
+// --------------------------------------------
+
+var email = ("My email Address is" + " mansoorihassan409@gmail.com");
+
+alert (email);
+
+console.log(email);
+
+// --------------------------------------------
+
+var book =
+  "I am trying to learn from the Book" +
+  " A smarter" +
+  "\n" +
+  "Way to learn JavaScript";
+
+alert (book);
+
+console.log(book);
+
+// --------------------------------------------
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+// --------------------------------------------
+
+var js = "|==============>>////// | \\\\\\<<================|";
+
+alert (js);
+document.write("|==============>>////// | \\\\\\<<================|")
