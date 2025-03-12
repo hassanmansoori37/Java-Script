ae9afae4ea02b8a0b64e2ff3928cb63c1596b43c
class12 <mansoorihassan409@gmail.com>
2025-03-12 10:35:27 +0500

    alert completed

---
+++ Assignment/class 1/app.js
@@ -0,0 +1,9 @@
+alert ("Error! Please enter a valid password.");
+ 
+alert ("Welcome to JS Land... Happy Cording!");
+
+alert ("Welcome to JS Land...");
+
+alert(" Happy Cording!");
+
+alert("Hello... I can run through my web browser's console");
