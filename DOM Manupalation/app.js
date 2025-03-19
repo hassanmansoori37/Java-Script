446a7c60702dd23687c6cddab858a26b39d68c91
class12 <mansoorihassan409@gmail.com>
2025-03-19 10:39:36 +0500

    dom manupulationn xompleted

--- DOM Manupalation/app.js
+++ DOM Manupalation/app.js
@@ -1,3 +1,54 @@
+// querySelector: // (Ye method kisi specific HTML element ko select karne ke liye istemal hota hai.)
+// .innerHTML     // (Ye property kisi HTML element ke content ko get ya set karne ke liye istemal hoti hai)
+
+// var head = document.querySelector("#head")
+// console.log(head.innerHTML)
+// head.innerHTML = "Muhammad Hassan"
+
+
+// -------------------------------------------------------------
+
+// var p = document.querySelector("#para");
+// p.innerHTML = "Hassan"
+
+
+// -------------------------------------------------------------
+
+// var h1 = document.querySelector("#head")
+
+// function greetuser() {
+
+// if (h1.innerHTML === "Hello DOM") {
+
+//   h1.innerHTML = "Change thorugh js";
+//   console.log (h1.innerHTML)
+
+// } else {
+
+//   h1.innerHTML = "Hello World";
+//   console.log(h1.innerHTML)
+
+// }
+
+// }
+
+
+// -------------------------------------------------------------
+
+
+
+// var head = document.querySelector("#head");
+// console.log(head.innerHTML = "Content Replace kia ha mena console me")
+
+// head.innerHTML = "Hello INNER HTML"
+// head.style.backgroundColor = "red"
+// head.style.color = "white"
+
+
+
+// -------------------------------------------------------------
+
+
 // var email = document.querySelector('#email');
 // var password = document.querySelector('#password');
 
